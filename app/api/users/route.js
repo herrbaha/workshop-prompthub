c31907f3526f3a73c6450fbb757468083e1637d5
herrbaha <baha_i@hotmail.com>
2024-02-23 16:18:06 +0100

    res geänderst

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(req, res) {
     let data = await req.json();
     console.log(data);
-    return NextResponse.json({res:'sen cok yasa'}, {status:203});
+    return NextResponse.json({res:'padisahim sen cok yasa'}, {status:203});
     } 
     // export async function GET(req, res) {
     //     let data2 = await req.json();
@@ -13,5 +13,5 @@ export async function POST(req, res) {
     
         export async function GET(req, res) {
             console.log("GET request received");
-            return NextResponse.json({ res: 'hepsiyle yasa' }, { status: 201 });
+            return NextResponse.json({ res: 'ben de yigit miyim?' }, { status: 201 });
         }
