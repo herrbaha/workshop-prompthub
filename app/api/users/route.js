8b7f80d1b393025b1178643efedfd55b91a450cb
herrbaha <baha_i@hotmail.com>
2024-02-24 14:09:09 +0100

    mehr datai gelöscht

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,17 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
-    let data = await req.json();
-    console.log(data);
-    return NextResponse.json({res:'padisahim sen cok yasa'}, {status:203});
-    } 
-    // export async function GET(req, res) {
-    //     let data2 = await req.json();
-    //     console.log(data2);
-    //     return NextResponse.json({res:'hepsiyle yasa'}, {status:201});
-    //     } 
-    
-        export async function GET(req, res) {
-            console.log("GET request received");
-            return NextResponse.json({ res: 'ben de yigit miyim?' }, { status: 201 });
-        }
+  let data = await req.json();
+  console.log(data);
+  return NextResponse.json({ res: "padisahim sen cok yasa" }, { status: 203 });
+}
+
+export async function GET(req, res) {
+  console.log("GET request received");
+  return NextResponse.json({ res: "ben de yigit miyim?" }, { status: 201 });
+}
